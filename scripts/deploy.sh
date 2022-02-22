7ca202738d0a7570863909a933addd3ab57a4980
David Bowland <dbowland1@gmail.com>
2022-02-21 23:59:54 -0600

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,13 +14,14 @@ sam build --template ${SAM_TEMPLATE} --use-container
 
 # Deploy build lambda
 
-TESTING_ARTIFACTS_BUCKET=aws-sam-cli-managed-jokes-api-tes-artifactsbucket-yjq9qs54j7qj
-TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/aws-sam-cli-managed-jokes-CloudFormationExecutionR-4HD5AU9RBZAQ"
+TESTING_ARTIFACTS_BUCKET=jokes-lambda-test
+TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/jokes-cloudformation-test"
 TESTING_STACK_NAME=jokes-api-test
 sam deploy --stack-name ${TESTING_STACK_NAME} \
            --capabilities CAPABILITY_IAM \
            --region us-east-2 \
            --s3-bucket ${TESTING_ARTIFACTS_BUCKET} \
+           --s3-prefix jokes-api-test \
            --no-fail-on-empty-changeset \
            --role-arn ${TESTING_CLOUDFORMATION_EXECUTION_ROLE} \
-           --parameter-overrides "AccountId=$AWS_ACCOUNT_ID Environment=test"
+           --parameter-overrides "Environment=test"
