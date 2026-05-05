e5759395a1714cb6e916a16623f7259e534ad319
David Bowland <dbowland1@gmail.com>
2026-05-04 22:25:27 -0500

    Split blob for jokes table

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,11 @@ export const apiUrl = process.env.API_URL as string
 export const dynamodbTableName = process.env.DYNAMODB_TABLE_NAME as string
 export const randomCountMaximum = parseInt(process.env.RANDOM_COUNT_MAXIMUM as string, 10)
 
+// ID Generation
+
+export const idMinLength = parseInt(process.env.ID_MIN_LENGTH as string, 10)
+export const idMaxLength = parseInt(process.env.ID_MAX_LENGTH as string, 10)
+
 // JsonPatch
 
 export const throwOnInvalidJsonPatch = true
